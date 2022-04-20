c9c883cb586e7a11c577cd7e35af00b384eea89b
Adrian Molina <adriansmc@gmail.com>
2022-04-20 01:03:44 -0600

    arreglando funciones

--- funciones.sql
+++ funciones.sql
@@ -52,10 +52,15 @@ begin
 end;
 
 
+
+/*
+ CREO QUE LE FALTAN VALIDACIONES
+ */
 DROP PROCEDURE IF EXISTS AddDefuncion;
 CREATE PROCEDURE AddDefuncion( dpi_D BIGINT, fecha_d varchar(20), motivo_m varchar(200))
 begin
     declare id_per int;
+    declare dpi_per int;
     declare gene char(1);
     declare nombre_m varchar(50);
     declare casado int;
@@ -70,6 +75,11 @@ begin
 
     select p.id, p.genero, p.nombre1 into id_per, gene, nombre_m
     from persona p
+    where p.cui = dpi_D;
+
+    select d.id into dpi_per
+    from dpi d
+    inner join persona p on p.id = d.id_persona
     where p.cui = dpi_D;
 
 
@@ -94,6 +104,7 @@ begin
         IF casado - divorciado = 1 THEN
             select dpi_mujer into dpi_mujer_div
             from matrimonio m
+            where m.dpi_hombre = dpi_per
             order by fecha_matrimonio DESC
             LIMIT 1;
 
@@ -106,13 +117,42 @@ begin
             set estado_civil = 4
             where id = id_mujer_div;
 
-            select CONCAT('Murio ', nombre_m, ' y ', nombre_d, ' quedo viud@') DEFUNCION;
+            select CONCAT('Murio ', nombre_m, ' y ', nombre_d, ' quedo viuda') DEFUNCION;
+        end if;
+    ELSEIF gene = 'F' THEN
+        select count(*) into casada
+        from matrimonio m
+            inner join dpi d on d.id = m.dpi_mujer
+            inner join persona p on p.id = d.id_persona
+        where p.cui = dpi_D;
+
+        select count(*) into divorciada
+        from divorcio di
+            inner join matrimonio m on m.id = di.id_matrimonio
+            inner join dpi d on d.id_persona = m.dpi_mujer
+            inner join persona p on p.id = d.id_persona
+        where p.cui = dpi_D;
+
+        IF casada - divorciada = 1 THEN
+            select dpi_hombre into dpi_hombre_div
+            from matrimonio m
+            where m.dpi_mujer = dpi_per
+            order by fecha_matrimonio DESC
+            LIMIT 1;
+
+            select p.id, p.nombre1 into id_hombre_div, nombre_d
+            from dpi d
+                inner join persona p on p.id = d.id_persona
+            where d.id = dpi_hombre_div;
+
+            update persona
+            set estado_civil = 4
+            where id = id_hombre_div;
+
+            select CONCAT('Murio ', nombre_m, ' y ', nombre_d, ' quedo viudo') DEFUNCION;
         end if;
     end if;
 end;
-
-call AddDefuncion(3020721280101, '2030-12-24', 'Desaparecio');
-select * from persona;
 
 
 DROP PROCEDURE IF EXISTS addMatrimonio;
@@ -121,31 +161,76 @@ begin
     declare id_dpi_h int;
     declare id_h int;
     declare e_h int;
+    declare gen_h char(1);
+    declare nombre1 varchar(50);
     declare id_dpi_m int;
     declare id_m int;
     declare e_m int;
+    declare gen_m char(1);
+    declare nombre2 varchar(50);
+    declare dpi_exist1 int;
+    declare dpi_exist2 int;
+    declare muerto1 int;
+    declare muerto2 int;
 
-    select d.id, p.id, p.estado_civil into id_dpi_h, id_h, e_h
+    select count(*) into dpi_exist1
     from dpi d
     inner join persona p on p.id = d.id_persona
     where p.cui = dpi_H;
 
-    select d.id, p.id, p.estado_civil into id_dpi_m, id_m, e_m
+    select count(*) into dpi_exist2
     from dpi d
     inner join persona p on p.id = d.id_persona
     where p.cui = dpi_M;
 
-    IF e_h = 2 or e_m = 2 THEN
-        select 'Ambos deben estar solteros antes de casarse';
-    ELSE
-        insert into matrimonio (fecha_matrimonio, dpi_hombre, dpi_mujer)
-        values (STR_TO_DATE(fecha_m, '%Y-%m-%d'), id_dpi_h, id_dpi_m);
+    select d.id, p.id, p.estado_civil, p.genero, p.nombre1 into id_dpi_h, id_h, e_h, gen_h, nombre1
+    from dpi d
+    inner join persona p on p.id = d.id_persona
+    where p.cui = dpi_H;
 
-        update persona p
-        set p.estado_civil = 2
-        where p.id = id_h or p.id = id_m;
+    select d.id, p.id, p.estado_civil, p.genero, p.nombre1 into id_dpi_m, id_m, e_m, gen_m, nombre2
+    from dpi d
+    inner join persona p on p.id = d.id_persona
+    where p.cui = dpi_M;
+
+    select count(*) into muerto1
+    from defuncion d
+    where d.id_persona = id_h;
+
+    select count(*) into muerto2
+    from defuncion d
+    where d.id_persona = id_m;
+
+    IF dpi_exist1 > 0 and dpi_exist2 > 0 THEN
+        IF e_h = 2 or e_m = 2 or e_h = 4 or e_m = 4 THEN
+            select 'Ambos deben estar solteros antes de casarse' AVISO;
+        ELSE
+            IF gen_h = 'M' and gen_m = 'F' THEN
+                IF muerto1 > 0 or muerto2 > 0 THEN
+                    select 'NO PUEDES CASARTE CON UN MUERTO' AVISO;
+                ELSE
+                    insert into matrimonio (fecha_matrimonio, dpi_hombre, dpi_mujer)
+                    values (STR_TO_DATE(fecha_m, '%Y-%m-%d'), id_dpi_h, id_dpi_m);
+
+                    update persona p
+                    set p.estado_civil = 2
+                    where p.id = id_h or p.id = id_m;
+
+                    select CONCAT('Se han casado ', nombre1, ' y ', nombre2) AVISO;
+                END IF;
+            ELSE
+                select 'Deben ser del genero opuesto para poder casarse' AVISO;
+            END IF;
+        END IF;
+    ELSE
+        select 'Ambos deben poseer DPI para casarse' AVISO;
     END IF;
 end;
+
+
+
+
+
 
 DROP PROCEDURE IF EXISTS addDivorcio;
 CREATE PROCEDURE addDivorcio(fecha_m varchar(20),  id_ma INT)
@@ -173,12 +258,3 @@ begin
     set estado_civil = 3
     where id = id_h or id = id_m;
 end;
-
-call AddMatrimonio(3020721280101, 3020721330101, '2026-12-12');
-call addDivorcio('2026-12-14', 2);
-
-select * from persona;
-select * from defuncion;
-select * from estado_civil;
-select * from matrimonio;
-select * from divorcio;
--- otro.sql
+++ otro.sql
@@ -1,9 +1,12 @@
 call addNacimiento(3020721280101, 3020721290101, 'Adrian', 'Alejandro', null, '2028-10-20', 101, 'M');
-call AddDefuncion(3020721290101, '2030-12-24', 'Desaparecio');
 
 call AddMatrimonio(3020721280101, 3020721290101, '2026-12-12');
 call addDivorcio('2026-12-14', 1);
-call AddMatrimonio(3020721280101, 3020721310101, '2026-12-14');
+call AddMatrimonio(3020721280101, 3020721310101, '2026-12-15');
+call AddMatrimonio(3020721320101, 3020721290101, '2026-12-18');
+
+call AddDefuncion(3020721280101, '2030-12-24', 'Desaparecio');
+call AddDefuncion(3020721290101, '2030-12-26', 'Desaparecio');
 
 select * from persona;
 select * from defuncion;
