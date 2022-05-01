7467fdedabcff3f3d435ac3599eb41a5723f92d5
Adrian Molina <adriansmc@gmail.com>
2022-04-30 19:08:11 -0600

    codigo Terminado

--- otro.sql
+++ otro.sql
@@ -62,7 +62,7 @@ call AddDefuncion(3020721290101, '2030-10-02', 'Murio por beber gasolina');
 -- Crean el acta 2 veces para asegurarse
 call AddDefuncion(3020721290101, '2030-10-02', 'Murio por beber gasolina');
 call AddDefuncion(3020721340101, '2030-10-02', 'Murio por fumar en una gasolinera con su hijo');
-call AddDefuncion(3020721400101, '2030-10-02', 'Tuvo la desgracia de tener a con poca inteligencia');
+call AddDefuncion(3020721400101, '2030-10-02', 'Tuvo la desgracia de tener a padres poco deductivos');
 
 
 
@@ -105,6 +105,77 @@ call addMatrimonio(3020721300101,3020721360101, '2024-02-08');
 call addDivorcio('2024-03-08', 9);
 call addMatrimonio(3020721310101,3020721350101, '2025-02-08');
 call addDivorcio('2025-03-08', 10);
+
+
+-- Intenta sacar licencia tipo A pero no lo dejan.
+call addLicencia(3020721300101, '2026-02-04', 'A');
+
+-- Le dice a sus amigos que tienen que sacar licencia C o M
+call addLicencia(3020721300101, '2026-02-04', 'C');
+call addLicencia(3020721310101, '2026-02-04', 'C');
+call addLicencia(3020721350101, '2026-02-04', 'M');
+call addLicencia(3020721360101, '2026-02-04', 'M');
+call addLicencia(3020721370101, '2026-02-04', 'C');
+
+-- Una intenta sacar una segunda licencia y la matrix no la deja
+call addLicencia(3020721370101, '2026-02-04', 'C');
+
+-- Los multan por hacer carreras.
+call anularLicencia(1, '2026-02-05', 'Hacer carreras en via publica');
+call anularLicencia(2, '2026-02-05', 'crear disturbios en via publica');
+call anularLicencia(3, '2026-02-05', 'agredir verbalmente a un oficial en via publica');
+call anularLicencia(4, '2026-02-05', 'fingir desmayo en via publica');
+call anularLicencia(5, '2026-02-05', 'Hacer carreras en via publica');
+
+-- Uno intenta ir a renovar su licencia al dia siguiente.
+call renewLicencia(1, '2026-02-06', 'C');
+
+-- Le cuenta a sus amigos que no pueden renovar sus licencias ese anio, entonces deciden fundar una pandilla de motociclistas.
+call addLicencia(3020721300101, '2026-02-04', 'E');
+call addLicencia(3020721310101, '2026-02-04', 'E');
+call addLicencia(3020721350101, '2026-02-04', 'E');
+call addLicencia(3020721360101, '2026-02-04', 'E');
+call addLicencia(3020721370101, '2026-02-04', 'E');
+
+
+-- Dos anios despues unos regresaron a renovar sus licencias
+call renewLicencia(1, '2028-02-06', 'C');
+call renewLicencia(2, '2028-02-06', 'C');
+call renewLicencia(5, '2028-02-06', 'C');
+
+-- Igual al anio siguiente.
+call renewLicencia(1, '2029-02-06', 'C');
+call renewLicencia(2, '2029-02-06', 'C');
+call renewLicencia(5, '2029-02-06', 'C');
+
+-- Al anio siguiente 2 de ellos decidieron que querian una licencia B pero uno queria la C de nuevo.
+call renewLicencia(1, '2030-02-06', 'B');
+call renewLicencia(2, '2030-02-06', 'B');
+call renewLicencia(5, '2030-02-06', 'C');
+
+-- En ese anio los 3 consiguieron un trabajo en el que era necesario tener licencia Tipo A.
+call renewLicencia(1, '2031-02-06', 'A');
+call renewLicencia(2, '2031-02-06', 'A');
+call renewLicencia(5, '2031-02-06', 'A');
+
+
+call getNacimiento(3020721380101);
+call getNacimiento(3020721390101);
+
+call getDpi(3020721280101);
+call getDpi(3020721330101);
+
+call getLicencias(3020721300101);
+call getLicencias(3020721370101);
+
+call getDivorcio(2);
+call getDivorcio(6);
+
+call getDefuncion(3020721280101);
+call getDefuncion(3020721340101);
+
+call getMatrimonio(1);
+call getMatrimonio(6);
 
 
 select * from persona;
